c21efebe9faafbd36cb7a51c0b8ebf9eb71f9db3
João Simão <101663962+joaosiwao@users.noreply.github.com>
2022-12-04 17:48:00 -0300

    Update TravelBooking.c

--- TravelBooking.c
+++ TravelBooking.c
@@ -3,67 +3,88 @@
 #include <stdlib.h>
 
 struct data {
-  int dia;
-  int mes;
-  int ano;
+    int dia;
+    int mes;
+    int ano;
+
+    bool operator<(const data& outro) {
+        if (ano < outro.ano){
+            return 1;
+        }
+        if (ano == outro.ano){
+            if (mes < outro.mes){
+                return 1;
+            }
+            if (mes == outro.mes && dia <= outro.dia){
+                return 1;
+            }
+        }
+        return 0;
+    }
+    bool operator==(const data& outro) {
+        if (ano == outro.ano && mes == outro.mes && dia == outro.dia){
+            return 1;
+        }
+        return 0;
+    }
 };
 
 struct voo {
-  int codigo;
-  char *origem;
-  char *destino;
+    int codigo;
+    char *origem;
+    char *destino;
 };
 
 struct no_voo {
-  Voo *voo;
-  struct no_voo *proximo;
+    Voo *voo;
+    struct no_voo *proximo;
 };
 
 struct lista_voo {
-  struct no_voo *primeiro;
+    struct no_voo *primeiro;
 };
 
 struct passageiro {
-  int id;
-  char *nome;
-  char *endereco;
+    int id;
+    char *nome;
+    char *endereco;
 };
 
 struct no_passageiro {
-  Passageiro *passageiro;
-  struct no_passageiro *proximo;
+    Passageiro *passageiro;
+    struct no_passageiro *proximo;
 };
 
 struct lista_passageiro {
-  struct no_passageiro *primeiro;
+    struct no_passageiro *primeiro;
 };
 
 struct reserva{
-  int codigo;
-  Data *data_viagem;
-  Passageiro *passageiro;
-  Voo *voo;
-  Assento assento;
+    int codigo;
+    Data *data_viagem;
+    Passageiro *passageiro;
+    Voo *voo;
+    Assento assento;
 };
 
 struct agenda {
-  Reserva *reserva;
-  Agenda *esq;
-  Agenda *dir;
+    Reserva *reserva;
+    Agenda *esq;
+    Agenda *dir;
 };
 
 struct trecho {
-  Reserva *reserva;
-  struct trecho *proximo;
+    Reserva *reserva;
+    struct trecho *proximo;
 };
 
 struct viagem {
-  struct trecho *trechos;
+    struct trecho *trechos;
 };
 
 struct tabela_viagem {
-  int tamanho;
-  Viagem *tabela_hash;
+    int tamanho;
+    Viagem *tabela_hash;
 };
 
 
@@ -71,35 +92,20 @@ struct tabela_viagem {
 
 
 //retorna 0 caso a data nova seja menor que a antiga (esquerda) e 1 caso a data nova seja maior ou igual a antiga (direita).
-int *checaData(Data Nova,Data Ant){
-  int diaN, mesN, anoN ;
-  int diaA, mesA, anoA ;
+int checaData(Data anterior, Data nova){
 
-  diaN = Nova.dia ;
-  mesN = Nova.mes ;
-  anoN = Nova.ano ;
-
-  diaA = Ant.dia ;
-  mesA = Ant.mes ;
-  anoA = Ant.ano ; 
-
-  if (anoA > anoN){
-    return 0;
-  }
-  if (anoA == anoN){
-    if (mesA > mesN){
-      return 0;
+    if (anterior.ano < nova.ano){
+        return 1;
     }
-    else if (mesA == mesN){
-      if (diaA > diaN){
-        return 0;
-      }
-  }
-  }
-  else{
-    return 1;
-  }
-  return NULL;
+    if (anterior.ano == nova.ano){
+        if (anterior.mes < nova.mes){
+            return 1;
+        }
+        if (anterior.mes == nova.mes && anterior.dia <= nova.dia){
+            return 1;
+        }
+    }
+    return 0;
 }
 
 Data hoje, *aux;
@@ -109,23 +115,17 @@ Data aux;
 /* Cria ABB */
 Agenda *IniciaABB(Reserva primeira){
 
-//Dados Invalidos: (não sei se a questao aux e *aux esta certa)
-aux = primeira.data_viagem;
-if (checaData(*aux, hoje) == 0){
-  return NULL; //data invalida (<hoje)
-}
-
-Reserva *auxiliar;
-*auxiliar = primeira;
+    //Dados Invalidos: (Data da viagem < Data de hoje)
+    if (checaData(hoje, primeira.data_viagem) == 0){
+        return NULL;
+    }
 
 //Happy path: (completamente perdido na questao dos ponteiros)
-  Agenda *raiz = (Agenda *)malloc(sizeof(Agenda));
-  raiz->reserva = auxiliar;
-  raiz->esq = (Agenda *)malloc(sizeof(Agenda));
-  raiz->dir = (Agenda *)malloc(sizeof(Agenda));
-  raiz->esq = NULL;
-  raiz->dir = NULL;
-  return raiz;
+    Agenda *raiz = (Agenda *)malloc(sizeof(Agenda));
+    raiz->reserva = &primeira;
+    raiz->esq = NULL;
+    raiz->dir = NULL;
+    return raiz;
 }
 
 
@@ -135,29 +135,27 @@ Reserva *auxiliar;
 
 
 Agenda *BuscaAgenda(Agenda *raiz, Data busca) {
-  Data *aux;
-  *aux = busca;
 
-  if (raiz != NULL) {
-    if (raiz->reserva->data_viagem == aux){
-      return raiz;
+    if (raiz != NULL) {
+        if (raiz->reserva->data_viagem == aux){
+            return raiz;
+        }
+
+        int checagemData;
+        Data inicial;
+        inicial = *raiz->reserva->data_viagem;
+
+        checagemData  = checaData(busca,inicial);
+
+
+        if (checagemData == 0) {
+            return BuscaAgenda(raiz->esq, busca);
+        } else {
+            return BuscaAgenda(raiz->dir, busca);
+        }
     }
-    
-    int checagemData;
-    Data inicial;
-    inicial = *raiz->reserva->data_viagem;
 
-    checagemData  = checaData(busca,inicial);
-
-
-    if (checagemData == 0) {
-      return BuscaAgenda(raiz->esq, busca);
-    } else {
-      return BuscaAgenda(raiz->dir, busca);
-    }
-  }
-
-  return NULL;
+    return NULL;
 }
 
 
@@ -167,74 +165,86 @@ Agenda *BuscaAgenda(Agenda *raiz, Data busca) {
  * resultante ou NULL raiz e o nó sejam NULL */
 
 Agenda *InsereAgenda(Agenda *raiz, Agenda *nova) {
-  if (raiz == NULL && nova == NULL) {
-    return NULL;
-  } else if (raiz == NULL) {
-    return nova;
-  }
+    if (raiz == NULL) {
+        return nova;
+    }
 
-  if (raiz->reserva->data_viagem < nova->reserva->data_viagem) {
-      raiz->esq = InsereAgenda(raiz->esq, nova);
-  } 
-    
-   else {
-      raiz->dir = InsereAgenda(raiz->dir, nova);
-  }
-  return raiz;
+    if (checaData(raiz->reserva->data_viagem, nova->reserva->data_viagem) == 1){
+        raiz->dir = InsereAgenda(raiz->dir, nova);
+    } else {
+        raiz->esq = InsereAgenda(raiz->esq, nova);
+    }
+    return raiz;
 
-  }
+}
 
 
 /*Realiza uma busca linear pelo código de reserva*/
 
 Agenda *BuscaCodigo(Agenda *raiz, int reserva){
-  if (raiz->reserva->codigo == reserva){
-    return raiz;
-  }
-  
-  if(raiz != NULL){
-        BuscaCodigo(raiz->esq,reserva);
-        BuscaCodigo(raiz->dir,reserva);
+    if (raiz == NULL){
+        return NULL;
     }
+    if (raiz->reserva->codigo == reserva){
+        return raiz;
+    }
+    Agenda *esq = BuscaCodigo(raiz->esq,reserva);
+    Agenda *dir = BuscaCodigo(raiz->dir,reserva);
+    if(esq != NULL){
+        return esq;
+    }
+    return dir;
 }
 
 /*Remove Agenda com um dado código de reserva*/
-
-
+bool RemoverAgenda() {
+    if (raiz == NULL) {
+        return NULL;
+    }
+    if (raiz->reserva->codigo == reserva) {
+        return raiz;
+    }
+    Agenda *esq = BuscaCodigo(raiz->esq, reserva);
+    Agenda *dir = BuscaCodigo(raiz->dir, reserva);
+    if (esq != NULL) {
+        return esq;
+    }
+    return dir;
+}
 
 
 /*Busca por (i) identificador do passageiro e código do voo*/
 
 Agenda *BuscaI(Agenda *raiz, Passageiro passageiro, int codigo){
-  Passageiro *aux;
-  *aux = passageiro;
-  if (raiz->reserva->codigo == codigo && raiz->reserva->passageiro == aux){
-    return raiz;
-  }
-  
-  if(raiz != NULL){
-        BuscaI(raiz->esq,passageiro,codigo);
-        BuscaI(raiz->dir,passageiro,codigo);
+    if (raiz == NULL){
+        return NULL;
     }
+    if (raiz->reserva->codigo == codigo && raiz->reserva->passageiro == passageiro){
+        return raiz;
+    }
+    Agenda *esq = BuscaI(raiz->esq,passageiro, codigo);
+    Agenda *dir = BuscaI(raiz->dir,passageiro, codigo);
+    if(esq != NULL){
+        return esq;
+    }
+    return dir;
 }
 
 /*Busca por (ii) identificador do passageiro e data da viagem.*/
 
 Agenda *BuscaII(Agenda *raiz, Passageiro passageiro, Data data){
-  Passageiro *aux;
-  *aux = passageiro;
-  
-  Data *auxi;
-  *auxi = data;
-
-  if (raiz->reserva->data_viagem == auxi && raiz->reserva->passageiro == aux){
-    return raiz;
-  }
-  
-  if(raiz != NULL){
-        BuscaII(raiz->esq,passageiro,data);
-        BuscaII(raiz->dir,passageiro,data);
+    if (raiz == NULL){
+        return NULL;
     }
+    if (raiz->reserva->data_viagem == data && raiz->reserva->passageiro == passageiro){
+        return raiz;
+    }
+    Agenda *esq = BuscaII(raiz->esq,passageiro,data);
+    Agenda *dir = BuscaII(raiz->dir,passageiro,data);
+    if(esq != NULL){
+        return esq;
+    }
+    return dir;
 }
 
 
@@ -242,9 +252,10 @@ Agenda *BuscaII(Agenda *raiz, Passageiro passageiro, Data data){
 /*Edita a reserva*/
 
 Agenda *Edita(Agenda *raiz, int codEditar){
-  Agenda *edita;
-  edita = BuscaCodigo(raiz,codEditar);
-  }
+    Agenda *edita;
+    edita = BuscaCodigo(raiz,codEditar);
+    /*I- Achar agenda; II- Criar Cópia; III- Remover Agenda original, IV- Inserir cópia modificada*/
+}
 
 
 
